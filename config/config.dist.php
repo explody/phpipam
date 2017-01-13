3cd3a57e449e9422adb364f36ebb8cb5a646ccef
Mike Culbertson <explody@explody.io>
2017-01-13 00:01:52 -0500

    A few steps towards refactoring config

--- config/config.dist.php
+++ config/config.dist.php
@@ -1,29 +1,8 @@
 <?php
 
-/* database connection details
- ******************************/
-$db['host'] = "localhost";
-$db['user'] = "phpipam";
-$db['pass'] = "phpipamadmin";
-$db['name'] = "phpipam";
-$db['port'] = 3306;
-
-/* SSL options for MySQL
- ******************************
- See http://php.net/manual/en/ref.pdo-mysql.php
-     https://dev.mysql.com/doc/refman/5.7/en/ssl-options.html
-
-     Please update these settings before setting 'ssl' to true.
-     All settings can be commented out or set to NULL if not needed
-
-     php 5.3.7 required
-*/
-$db['ssl']        = false;                           # true/false, enable or disable SSL as a whole
-$db['ssl_key']    = "/path/to/cert.key";             # path to an SSL key file. Only makes sense combined with ssl_cert
-$db['ssl_cert']   = "/path/to/cert.crt";             # path to an SSL certificate file. Only makes sense combined with ssl_key
-$db['ssl_ca']     = "/path/to/ca.crt";               # path to a file containing SSL CA certs
-$db['ssl_capath'] = "/path/to/ca_certs";             # path to a directory containing CA certs
-$db['ssl_cipher'] = "DHE-RSA-AES256-SHA:AES128-SHA"; # one or more SSL Ciphers
+// Do not change these
+require_once dirname(__FILE__) . '/version.php';
+require_once dirname(__FILE__) . '/constants.php';
 
 /**
  * php debugging on/off
@@ -31,7 +10,7 @@ $db['ssl_cipher'] = "DHE-RSA-AES256-SHA:AES128-SHA"; # one or more SSL Ciphers
  * true  = SHOW all php errors
  * false = HIDE all php errors
  ******************************/
-$debugging = false;
+$debugging = true;
 
 /**
  *  manual set session name for auth
@@ -39,23 +18,6 @@ $debugging = false;
  *  optional
  */
 $phpsessname = "phpipam";
-
-/**
- *	BASE definition if phpipam
- * 	is not in root directory (e.g. /phpipam/)
- *
- *  Also change
- *	RewriteBase / in .htaccess
- ******************************/
-if(!defined('BASE'))
-define('BASE', "/");
-
-/**
- * Multicast unique mac requirement - section or vlan
- */
-
-if(!defined('MCUNIQUE'))
-define('MCUNIQUE', "section");
 
 /**
  * Permit private subpages - private apps under /app/tools/custom/<custom_app_name>/index.php
@@ -74,6 +36,7 @@ $private_subpages = array();
  *  from overview tab, so go to Credentials tab and make an API key for your project.
  */
 $gmaps_api_key = "";
+
 
 /*  proxy connection details
  ******************************/
@@ -103,5 +66,8 @@ elseif ($proxy_enabled == true && $proxy_use_auth == true) {
 
 /* for debugging proxy config uncomment next line */
 #var_dump(stream_context_get_options(stream_context_get_default()));
+
+// Include database settings
+require dirname(__FILE__) . '/database.php';
 
 ?>
